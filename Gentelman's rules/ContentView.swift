191bc4ca8ecf340b37bdaf0a50d5adecffbe11f8
Serge <volokserge@gmail.com>
2023-04-13 09:01:54 +0400

    Add viewwillappear

--- Gentelman's rules/ContentView.swift
+++ Gentelman's rules/ContentView.swift
@@ -17,5 +17,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        viewWillAppear(Bool)
+        //
     }
 }
