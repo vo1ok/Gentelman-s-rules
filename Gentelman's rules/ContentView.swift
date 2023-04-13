4a09c2068904eaae2383c93e7eaf27033a97c046
Serge <volokserge@gmail.com>
2023-04-13 08:48:45 +0400

    Initial Commit

---
+++ Gentelman's rules/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gentelman's rules
+//
+//  Created by Serge on 13.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gentelman's rules/Gentelman_s_rulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gentelman_s_rulesApp.swift
+//  Gentelman's rules
+//
+//  Created by Serge on 13.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Gentelman_s_rulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
